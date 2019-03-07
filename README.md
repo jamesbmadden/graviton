ebaba6d7cc4ee0aaa49ff0f13293ad4955fbedd7
James Madden <jamesbmadden@gmail.com>
2019-03-06 16:39:25 -0800

    Create Graviton Package

---
+++ README.md
@@ -0,0 +1,23 @@
+# Graviton
+Web Components Designed by James Madden, Built with LitElement, Loosely Following Material Spec.
+
+## Install
+```
+git clone https://github.com/jamesbmadden/graviton
+cd graviton
+yarn install
+```
+
+## Build
+```
+yarn build
+```
+
+## Use in a Project
+```
+yarn link
+```
+Then, in your project's directory, run
+```
+yarn link graviton
+```
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    button: './src/gvt-button.js',
+    input: './src/gvt-input.js',
+    dropdown: './src/gvt-dropdown.js',
+    panel: './src/gvt-panel.js'
+  },
+  mode: 'production',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'components')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-decorators'
+            ]
+          }
+        },
+        include: [
+          path.join(__dirname, '../src'),
+          /\/node_modules\/lit-element/
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader'
+        },
+        {
+          loader: 'less-loader'
+        }]
+      }
+    ]
+  }
+}
