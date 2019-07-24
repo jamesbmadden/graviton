cd702178da49072b90ed3cefd85fe74cf96fbc0a
James Madden <jamesbmadden@gmail.com>
2019-07-24 13:59:17 -0700

    Make it possible to set the name attribute of the input

--- input/src/gvt-input.js
+++ input/src/gvt-input.js
@@ -11,7 +11,8 @@ export class GravitonInput extends LitElement {
     return {
       type: String,
       autocomplete: String,
-      value: String
+      value: String,
+      name: String
     }
   }
   static get styles() {
@@ -29,8 +30,8 @@ export class GravitonInput extends LitElement {
         <input .value=${this.value} @input=${(e)=> {
           this.value = e.target.value;
           this.dispatchEvent(new Event('input'));
-        }} @change=${(e)=>this.dispatchEvent(new Event('change'))} class="fieldInput" name="fieldInput" id="fieldInput" placeholder=" " type="${this.type}" autocomplete="${this.autocomplete}">
-        <label class="fieldInputLabel" for="fieldInput"><slot></slot></label>
+        }} @change=${(e)=>this.dispatchEvent(new Event('change'))} class="fieldInput" name=${this.name} id=${this.name} placeholder=" " type="${this.type}" autocomplete="${this.autocomplete}">
+        <label class="fieldInputLabel" for=${this.name}><slot></slot></label>
       </div>`;
   }
 }
